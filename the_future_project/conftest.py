1f3499ae32e7aa2a17b612de7542db147a74eea2
Biola <gbozee@gmail.com>
2022-05-18 10:43:39 +0100

    initial commit

---
+++ the_future_project/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from the_future_project.users.models import User
+from the_future_project.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
